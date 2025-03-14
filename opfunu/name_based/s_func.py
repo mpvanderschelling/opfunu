cb5646fbf2403f67c7ca06772fa0a9011c359af4
Martin van der Schelling <61459087+mpvanderschelling@users.noreply.github.com>
2025-03-14 10:49:23 +0100

    added Sphere benchmark function with evaluation method

--- opfunu/name_based/s_func.py
+++ opfunu/name_based/s_func.py
@@ -54,3 +54,37 @@ class Salomon(Benchmark):
         self.n_fe += 1
         u = np.sqrt(np.sum(x ** 2))
         return 1 - np.cos(2 * np.pi * u) + 0.1 * u
+
+
+class Sphere(Benchmark):
+    name = "Sphere Function"
+    latex_formula = r'f_{\text{Sphere}}(x) = 1 - \cos \left (2 \pi \sqrt{\sum_{i=1}^{n} x_i^2} \right) + 0.1 \sqrt{\sum_{i=1}^n x_i^2}'
+    latex_formula_dimension = r'd = n'
+    latex_formula_bounds = r'x_i \in [-10, 10, ..., 10]'
+    latex_formula_global_optimum = r'f(0, 0, ...,0) = 1.0'
+    continuous = True
+    linear = False
+    convex = True
+    unimodal = True
+    separable = True
+
+    differentiable = True
+    scalable = True
+    randomized_term = False
+    parametric = False
+
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+
+    def __init__(self, ndim=None, bounds=None):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 2
+        self.check_ndim_and_bounds(ndim, bounds, np.array([[-5.12, 5.12] for _ in range(self.dim_default)]))
+        self.f_global = 0.
+        self.x_global = np.zeros(self.ndim)
+
+    def evaluate(self, x, *args):
+        self.check_solution(x)
+        self.n_fe += 1
+        res = np.sum(x**2)
+        return res
